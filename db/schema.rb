4e305d75fab76ea06d03ac0f577ca6d42cf300f4
ryotaro960 <kasakasaaroe@yahoo.co.jp>
2024-10-24 13:01:00 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -40,11 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_24_020727) do
   end
 
   create_table "comments", charset: "utf8mb3", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "movie_tags", charset: "utf8mb3", force: :cascade do |t|
@@ -113,6 +115,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_24_020727) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "post_movie_tags", "movie_tags"
   add_foreign_key "post_movie_tags", "posts"
   add_foreign_key "post_talent_tags", "posts"
